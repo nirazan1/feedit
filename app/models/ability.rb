a83c271ab7e02754fad2c62cf5a580fe75d61be9
Nirazan Pokhrel <nirazan@nirazan.com.np>
2015-05-08 14:36:38 +0545

    admin control added with rails_admin gem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,8 @@ class Ability
 
   def initialize(user)
     can :read, :all
+    can :access, :rails_admin   # grant access to rails_admin
+    can :dashboard              # grant access to the dashboard
     can :create , Feed
     can :update , Feed do |feed|
         feed.try(:name) == user.first_name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506083051) do
+ActiveRecord::Schema.define(version: 20150508080411) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150506083051) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
