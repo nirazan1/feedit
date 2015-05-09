ee8e6fb40088af28c7bc492c42bdd8ff1aecc490
Nirazan Pokhrel <nirazan@nirazan.com.np>
2015-05-09 10:59:42 +0545

    new comment button with pluralize method

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,7 +2,7 @@ class Feed < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :name, presence: true,
                     length: { minimum: 5 }
-  validates :text, presence: true
+  validates :text, presence: true,length: { within: 1..140 }
   belongs_to :user
   acts_as_votable
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, :only => [:show]
+  
   resources :feeds do
     resources :comments
   end
