cbe7a817df1d603a66ef7b3cf99ef963b843ec7e
Nirazan Pokhrel <nirazan@nirazan.com.np>
2015-05-09 13:37:11 +0545

    added about me field on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
       def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :avatar, :avatar_cache) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :current_password, :avatar, :avatar_cache) }
+          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :about_me, :password, :avatar, :avatar_cache) }
+          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :about_me, :password, :current_password, :avatar, :avatar_cache) }
       end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508080411) do
+ActiveRecord::Schema.define(version: 20150509070203) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150508080411) do
     t.datetime "updated_at"
     t.string   "avatar"
     t.boolean  "admin"
+    t.text     "about_me"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
