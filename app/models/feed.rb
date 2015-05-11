c3fd155fc8348bb859c67c292b36867793f9758b
Nirazan <nirazan@nirazan.com.np>
2015-05-11 22:35:49 +0545

    update user profile page and updated navbar

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,7 +1,7 @@
 class Feed < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	
-  validates :text, presence: true,length: { within: 1..140 }
+  validates :text, presence: true,length: { within: 1..200 }
   belongs_to :user
   acts_as_votable
 
