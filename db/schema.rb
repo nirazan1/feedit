14b27cd27d3c2f6a8abd948ccd35e48273465f0b
Nirazan <nirazan@nirazan.com.np>
2015-06-26 23:08:30 +0545

    minor chage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519120014) do
+ActiveRecord::Schema.define(version: 20150627171256) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -55,8 +55,13 @@ ActiveRecord::Schema.define(version: 20150519120014) do
     t.boolean  "admin"
     t.text     "about_me"
     t.datetime "last_seen_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
