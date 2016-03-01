cec128789948e641289f55037ac3674637092307
Nirajan <nirazan@nirazan.com.np>
2016-03-01 10:02:51 +0545

    pusher brance merged

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',       # required
-      :aws_access_key_id      => 'AKIAI4LR2XYQOYDZDY6Q',       # required
-      :aws_secret_access_key  => '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',       # required
-      :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
+      :aws_access_key_id      => 'AKIAIMCHXT5CTFTIWOPA',       # required
+      :aws_secret_access_key  => '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',       # required
+      :region                 => 'ap-southeast-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'tutemate-production'                     # required
+  config.fog_directory  = 'feedits3'                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
--- db/migrate/20160226050405_add_sender_id_to_notification.rb
+++
@@ -1,5 +0,0 @@
-class AddSenderIdToNotification < ActiveRecord::Migration
-  def change
-    add_reference :notifications, :sender, index: true, foreign_key: true
-  end
-end
