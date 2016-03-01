75605254e48b7b3b967b993669e2b178f8c735d3
Nirajan <nirazan@nirazan.com.np>
2016-03-01 10:02:51 +0545

    pusher brance merged

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -39,7 +39,7 @@ RailsAdmin.config do |config|
   end
 
   config.model User do
-    exclude_fields :feeds, :comments,:reset_password_sent_at,:remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+    exclude_fields :feeds, :comments,:reset_password_sent_at,:remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip , :password, :password_confirmation
     list do
       field :first_name
       field :last_name
