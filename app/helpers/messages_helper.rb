9be92405c5e29d0d27511901cabdd9d0e472637f
Nirajan Pokharel <nirazan@nirazan.com.np>
2017-03-30 01:56:32 +0545

    notification fix

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -6,4 +6,15 @@ module MessagesHelper
   def message_interlocutor(message)
     message.user == message.conversation.sender ? message.conversation.sender : message.conversation.recipient
   end
+
+  def day_in_words_or_date(datetime)
+    return 'n/a' if datetime.blank?
+    if datetime.today?
+      datetime.strftime('%H:%M %p')
+    elsif datetime.to_date == Date.yesterday
+      'Yesterday @ ' + datetime.strftime('%H:%M %p')
+    else
+      datetime.strftime('%l:%M %p, %b %e (%a)')
+    end
+  end
 end
