dc97491df4b437501b858f6a28d225aee7cebb2a
Nirajan Pokharel <nirazan@nirazan.com.np>
2017-04-01 14:24:11 +0545

    notification message fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,19 +2,34 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
 
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  process :tags => ['user_avatar']
+
+  version :standard do
+    process :eager => true
+    process :resize_to_fill => [100, 150, :north]
+  end
+
+  version :thumbnail do
+    eager
+    resize_to_fit(50, 50)
+  end
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
